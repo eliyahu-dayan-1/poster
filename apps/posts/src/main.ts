6a8722d5a4426821c41937447d692bdce3c9982a
eliyhau <Eliyhau@edea.co.il>
2022-02-25 13:56:46 +0200

    fix bodyparse.json

--- apps/posts/src/main.ts
+++ apps/posts/src/main.ts
@@ -1,4 +1,4 @@
-import * as bodyParser from 'body-parser';
+import { json } from 'body-parser';
 import * as cookieParser from 'cookie-parser';
 import * as cors from 'cors';
 import * as express from 'express';
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   };
   app.use(cors(corsOptions));
 }
-app.use(bodyParser.json());
+app.use(json());
 app.use(cookieParser());
 app.use('/api/post', postRoutes);
 const server = app.listen(port, () => {
