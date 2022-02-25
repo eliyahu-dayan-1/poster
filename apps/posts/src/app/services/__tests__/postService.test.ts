f515d4654f7fcc8920660bc9141765513c88fed2
eliyhau <Eliyhau@edea.co.il>
2022-02-25 13:43:13 +0200

    insert add test to comment

--- apps/posts/src/app/services/__tests__/postService.test.ts
+++ apps/posts/src/app/services/__tests__/postService.test.ts
@@ -1,12 +1,12 @@
 import { add } from '../postService';
 
 describe('check add function', () => {
-  test('basic check', async () => {
-    await add({
-      title: 'good morning',
-      Description: 'aaaa bbb',
-      imageId: 'aaa',
-      user: 'gggg',
-    });
-  });
+  // test('basic check', async () => {
+    // await add({
+    //   title: 'good morning',
+    //   Description: 'aaaa bbb',
+    //   imageId: 'aaa',
+    //   user: 'gggg',
+    // });
+  // });
 });
