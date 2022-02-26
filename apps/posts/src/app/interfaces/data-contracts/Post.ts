f4a36eb96d68a4b6206f408736a0b9f57b7a1f2f
eliyhau <Eliyhau@edea.co.il>
2022-02-26 19:07:57 +0200

    add post interface

--- apps/posts/src/app/interfaces/data-contracts/Post.ts
+++ apps/posts/src/app/interfaces/data-contracts/Post.ts
@@ -1,7 +1,7 @@
 export default interface Post {
   _id: string;
   title: string;
-  Description: string;
-  imageId: string;
-  user: string;
+  description: string;
+  image_id: string;
+  user_id: string;
 }
