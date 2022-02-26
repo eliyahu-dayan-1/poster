5d515ae59ee769a1427e699fdd3e54cd70e2fc66
eliyhau <Eliyhau@edea.co.il>
2022-02-26 13:47:56 +0200

    create collection service

--- apps/posts/src/app/controller/controller.ts
+++ apps/posts/src/app/controller/controller.ts
@@ -1,23 +1,23 @@
-import * as postService from '../services/postService';
+import { postCollectionService } from '../services/postService';
 import { Request, Response } from 'express';
 
 export async function getPost(req: Request, res: Response) {
-  const post = await postService.getById(req.params.id);
+  const post = await postCollectionService.getById(req.params.id);
   res.send(post);
 }
 
 export async function getPostsByUserId(req: Request, res: Response) {
-  const post = await postService.getByUserId(req.params.user_id);
+  const post = await postCollectionService.getByUserId(req.params.user_id);
   res.send(post);
 }
 
 export async function getPosts(req: Request, res: Response) {
-  const posts = await postService.query(req.query);
+  const posts = await postCollectionService.query(req.query);
   res.send(posts);
 }
 
 export async function deletePost(req: Request, res: Response) {
-  await postService.remove(req.params.id);
+  await postCollectionService.remove(req.params.id);
   res.end();
 }
 
@@ -25,7 +25,7 @@ export async function updatePost(req: Request, res: Response) {
   const post = req.body;
 
   try {
-    await postService.update(post, req.params.id);
+    await postCollectionService.update(post, req.params.id);
     res.send(post);
   } catch (err) {
     res.status(500).send(err);
@@ -34,7 +34,7 @@ export async function updatePost(req: Request, res: Response) {
 export async function addPost(req: Request, res: Response) {
   const post = req.body;
   try {
-    const addedPost = await postService.add(post);
+    const addedPost = await postCollectionService.add(post);
     res.send(addedPost);
   } catch (err) {
     res.status(500).send(err);
--- apps/posts/src/app/services/postService.ts
+++ apps/posts/src/app/services/postService.ts
@@ -1,98 +1,118 @@
 import * as dbService from '../../services/db.service';
-import { Collection, ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import { postLogger } from '../../services/logger.service';
 
-const COLLECTION = 'posts';
+const COLLECTION_NAME = 'posts';
+const DB_NAME = 'poster_posts';
 
-export async function query(filterBy = {}) {
-  const criteria = _buildCriteria(filterBy);
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      const posts = await collection.find(criteria).toArray();
-
-      return posts;
-    } catch (err) {
-      postLogger.error('ERROR: cannot find posts');
-      throw err;
-    }
-  }
+interface Constractor {
+  collectionName: string;
+  dbName: string;
 }
-
-export async function getById(postId: string) {
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      const post = await collection.findOne({ _id: new ObjectId(postId) });
-
-      return post;
-    } catch (err) {
-      postLogger.error(`ERROR: while finding post ${postId}`);
-      throw err;
-    }
+class CollectionService {
+  constructor({ collectionName, dbName }: Constractor) {
+    this.collectionName = collectionName;
+    this.dbName = dbName;
   }
-}
+  collectionName: string;
+  dbName: string;
+  query = async (filterBy = {}) => {
+    const criteria = this._buildCriteria(filterBy);
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        const posts = await collection.find(criteria).toArray();
 
-export async function getByUserId(userId: string) {
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      const posts = await collection.find({ user: userId }).toArray();
-
-      return posts;
-    } catch (err) {
-      postLogger.error(`ERROR: while get post By User Id ${userId}`);
-      throw err;
+        return posts;
+      } catch (err) {
+        postLogger.error('ERROR: cannot find posts');
+        throw err;
+      }
     }
-  }
-}
+  };
 
-export async function remove(postId: string) {
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      await collection.deleteOne({ _id: new ObjectId(postId) });
-    } catch (err) {
-      postLogger.error(`ERROR: cannot remove post ${postId}`);
-      throw err;
+  getById = async (postId: string) => {
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        const post = await collection.findOne({ _id: new ObjectId(postId) });
+
+        return post;
+      } catch (err) {
+        postLogger.error(`ERROR: while finding post ${postId}`);
+        throw err;
+      }
     }
-  }
-}
+  };
 
-export async function update(post, id: string) {
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      await collection.replaceOne({ _id: new ObjectId(id) }, { $set: post });
+  getByUserId = async (userId: string) => {
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        const posts = await collection.find({ user: userId }).toArray();
 
-      return post;
-    } catch (err) {
-      postLogger.error(`ERROR: cannot update post ${id}`);
-      postLogger.error(err);
-      throw err;
+        return posts;
+      } catch (err) {
+        postLogger.error(`ERROR: while get post By User Id ${userId}`);
+        throw err;
+      }
     }
-  }
-}
-export async function add(post) {
-  const collection = await dbService.getCollection(COLLECTION);
-  if (collection) {
-    try {
-      await collection.insertOne(post);
-      return post;
-    } catch (err) {
-      postLogger.error(`ERROR: cannot insert post`);
-      throw err;
-    }
-  }
-}
+  };
 
-function _buildCriteria(filterBy: any) {
-  const criteria: any = {};
-  // if (filterBy.txt) {
-  //   criteria.postName = filterBy.txt;
-  // }
-  /* if (filterBy.minBalance) {
+  remove = async (postId: string) => {
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        await collection.deleteOne({ _id: new ObjectId(postId) });
+      } catch (err) {
+        postLogger.error(`ERROR: cannot remove post ${postId}`);
+        throw err;
+      }
+    }
+  };
+
+  update = async (post, id: string) => {
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        await collection.replaceOne({ _id: new ObjectId(id) }, { $set: post });
+
+        return post;
+      } catch (err) {
+        postLogger.error(`ERROR: cannot update post ${id}`);
+        postLogger.error(err);
+        throw err;
+      }
+    }
+  };
+  add = async (post) => {
+    const collection = await dbService.getCollection(this.collectionName);
+    if (collection) {
+      try {
+        await collection.insertOne(post);
+        return post;
+      } catch (err) {
+        postLogger.error(`ERROR: cannot insert post`);
+        throw err;
+      }
+    }
+  };
+
+  _buildCriteria = (filterBy: any) => {
+    const criteria: any = {};
+    // if (filterBy.txt) {
+    //   criteria.postName = filterBy.txt;
+    // }
+    /* if (filterBy.minBalance) {
         criteria.balance = { $gte: +filterBy.minBalance }
     } */
-  return criteria;
+    return criteria;
+  };
 }
+
+const postCollectionService = new CollectionService({
+  collectionName: COLLECTION_NAME,
+  dbName: DB_NAME,
+});
+
+export { postCollectionService };
