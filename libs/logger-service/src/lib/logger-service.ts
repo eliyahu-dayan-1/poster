0e6c1b2ddff8870659135e6170e3e218769e6e31
eliyhau <Eliyhau@edea.co.il>
2022-02-26 13:20:31 +0200

    add logger service

---
+++ libs/logger-service/src/lib/logger-service.ts
@@ -0,0 +1,42 @@
+import * as mongodb from 'mongodb';
+import * as fs from 'fs';
+class Logger {
+  logOutputDir;
+  constructor(logOutputDir: string) {
+    this.logOutputDir = logOutputDir;
+    if (!fs.existsSync(this.logOutputDir)) {
+      fs.mkdirSync(this.logOutputDir);
+    }
+  }
+
+  getTime() {
+    const now = new Date();
+    return now.toUTCString();
+  }
+
+  doLog(line: any, level = 'Debug', shouldConsoleLogEither = false) {
+    if (typeof line !== 'string') line = JSON.stringify(line);
+    line = `[${this.getTime()}] - ${level} - ${line}\n`;
+    if (shouldConsoleLogEither) console.log(line);
+    fs.appendFileSync(`${this.logOutputDir}/backend.log`, line);
+  }
+
+  debug(line: any, shouldConsoleLogEither = false) {
+    this.doLog(line, 'Debug', shouldConsoleLogEither);
+  }
+  info(line: any, shouldConsoleLogEither = false) {
+    this.doLog(line, 'Info', shouldConsoleLogEither);
+  }
+  warn(line: any, shouldConsoleLogEither = false) {
+    this.doLog(line, 'Warn', shouldConsoleLogEither);
+  }
+  error(line: any, shouldConsoleLogEither = false) {
+    this.doLog(line, 'Error', shouldConsoleLogEither);
+  }
+
+  log(line: any, shouldConsoleLogEither = false) {
+    this.doLog(line, 'log', shouldConsoleLogEither);
+  }
+}
+
+export default Logger;
